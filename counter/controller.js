8db1576c3004e1370266edbb251cfc9e649c06d1
Nikolay Elgin <nikolka-net@mail.ru>
2021-07-31 23:12:29 +0600

    Добавил паттерн по mvc

---
+++ counter/controller.js
@@ -0,0 +1,26 @@
+import model from "./model.js";
+import view from "./view.js";
+
+const btnMinus = document.getElementById('btnMinus');
+const btnReset = document.getElementById('btnReset');
+const btnPlus = document.getElementById('btnPlus');
+
+view.updateCounter(model.counter);
+
+btnPlus.addEventListener('click', function () {
+	model.increase();
+	view.updateCounter(model.counter);
+
+});
+
+btnMinus.addEventListener('click', function () {
+	model.decrease();
+	view.updateCounter(model.counter);
+
+});
+
+btnReset.addEventListener('click', function () {
+	model.reset();
+	view.updateCounter(model.counter);
+
+});
